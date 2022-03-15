0c00023a36048e14cd1cbcbc4a5dff6cb3fb590d
MariaVivancos <mariavivancos@maria.vivancos>
2022-03-15 13:44:46 +0100

    Added calendar fragment in folder.

--- app/src/main/java/com/switcherette/plantapp/calendar/CalendarFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/calendar/CalendarFragment.kt
@@ -1,17 +1,19 @@
 package com.switcherette.plantapp.calendar
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.Fragment
+import com.switcherette.plantapp.R
 import com.switcherette.plantapp.databinding.FragmentCalendarBinding
 
-class CalendarFragment : Fragment() {
+class CalendarFragment: Fragment(R.layout.fragment_calendar){
 
     private lateinit var binding: FragmentCalendarBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding = FragmentCalendarBinding.bind(view)
+
+
     }
 }
