6e12851f62704639e8135afbe49344c9aae41a75
switcherette <gfornons@gmail.com>
2022-03-15 13:48:19 +0100

    added retrofit for PlantId

---
+++ app/src/main/java/com/switcherette/plantapp/data/PlantId.kt
@@ -0,0 +1,6 @@
+package com.switcherette.plantapp.data
+
+data class PlantId(
+    val plant_name: String,  //"latinName"
+    val probability: Double
+)
--- app/src/main/java/com/switcherette/plantapp/utils/utils.kt
+++ app/src/main/java/com/switcherette/plantapp/utils/utils.kt
@@ -1,4 +1,17 @@
 package com.switcherette.plantapp.utils
 
-class utils {
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.io.File
+import java.io.FileInputStream
+import java.util.Base64.getEncoder
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun convertToBase64(fileString: String): String {
+    val file = File(fileString)
+    val fis = FileInputStream(file)
+    return getEncoder().encodeToString(fis.readBytes())
 }
+
+
