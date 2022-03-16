e95b907381a40bcb45348936d0908af3c79f4b67
MariaVivancos <mariavivancos@maria.vivancos>
2022-03-16 13:42:50 +0100

    Auth with FirebaseUI working.

--- app/src/main/java/com/switcherette/plantapp/profile/MyProfileFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/profile/MyProfileFragment.kt
@@ -2,7 +2,10 @@ package com.switcherette.plantapp.profile
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
 import com.switcherette.plantapp.R
 import com.switcherette.plantapp.databinding.FragmentMyProfileBinding
 import com.switcherette.plantapp.databinding.FragmentPlantFormBinding
@@ -15,5 +18,19 @@ class MyProfileFragment : Fragment(R.layout.fragment_my_profile) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMyProfileBinding.bind(view)
+
+        with(binding) {
+            btnSignOut.setOnClickListener {
+                AuthUI.getInstance()
+                    .signOut(requireContext())
+                    .addOnCompleteListener {
+                        Toast.makeText(requireContext(), "Signed out", Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.action_myProfileFragment_to_loginFragment)
+                    }
+
+            }
+        }
     }
+
+
 }
