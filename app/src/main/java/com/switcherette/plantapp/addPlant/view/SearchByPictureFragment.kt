a4e310ced75d8e6b3b33422d58a6c14c9bc24667
switcherette <gfornons@gmail.com>
2022-03-20 15:31:06 +0100

    some design changes

--- app/src/main/java/com/switcherette/plantapp/addPlant/view/SearchByPictureFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/addPlant/view/SearchByPictureFragment.kt
@@ -1,17 +1,25 @@
 package com.switcherette.plantapp.addPlant.view
 
 
+import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.switcherette.plantapp.R
 import com.switcherette.plantapp.addPlant.adapter.SuggestionsAdapter
 import com.switcherette.plantapp.addPlant.viewModel.SearchByPictureViewModel
 import com.switcherette.plantapp.databinding.FragmentSearchByPictureBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class SearchByPictureFragment : Fragment(R.layout.fragment_search_by_picture) {
 
@@ -57,7 +65,7 @@ class SearchByPictureFragment : Fragment(R.layout.fragment_search_by_picture) {
                     val recyclerView = binding.rvSuggestions
 
                     recyclerView.layoutManager =
-                        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                        LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
                     val suggestionsAdapter = SuggestionsAdapter(it.suggestions) {
                         findNavController().navigate(R.id.action_searchByPictureFragment_to_plantFormFragment,
@@ -80,3 +88,8 @@ class SearchByPictureFragment : Fragment(R.layout.fragment_search_by_picture) {
         }
     }
 }
+
+
+
+
+
--- app/src/main/java/com/switcherette/plantapp/login/LoginFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/login/LoginFragment.kt
@@ -58,7 +58,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                 val signInIntent = AuthUI.getInstance()
                     .createSignInIntentBuilder()
                     .setAvailableProviders(providers)
-                    //.setLogo(R.drawable.plant_logo)
+                    .setLogo(R.drawable.plapp_logo)
                     .setTheme(R.style.LoginTheme)
                     .build()
                 signInLauncher.launch(signInIntent)
