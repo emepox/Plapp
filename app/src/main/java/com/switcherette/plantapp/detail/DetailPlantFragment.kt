6f4094596b5cac00ca153bafee065ed55d8234c6
MariaVivancos <mariavivancos@maria.vivancos>
2022-03-21 21:02:24 +0100

    added elvis operator

--- app/src/main/java/com/switcherette/plantapp/detail/DetailPlantFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/detail/DetailPlantFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.View.VISIBLE
+import android.widget.TextView
+import androidx.core.view.isVisible
 import com.switcherette.plantapp.R
 import com.switcherette.plantapp.data.UserPlant
 import com.switcherette.plantapp.databinding.FragmentDetailPlantBinding
@@ -21,19 +24,21 @@ class DetailPlantFragment : Fragment(R.layout.fragment_detail_plant) {
 
         plant = arguments?.getParcelable("plant")!!
 
-        Log.e("myplant", plant.toString())
+        Log.e("myplant", plant.family.toString())
 
         with(binding) {
+
             tvDetailNickname.text = plant.nickname
-            tvDetailCommonName.text = plant.commonName
+            tvDetailCommonName.text = plant.commonName?: "No common name data"
             tvCareWatering.text = plant.water.toString()
             tvCareLight.text = plant.light.toString()
-            tvDetailScientificName.text = plant.scientificName
-            tvDetailFamily.text = plant.family
-            tvDetailDescription.text = plant.description
-            tvDetailCultivation.text = plant.cultivation
-
+            tvDetailScientificName.text = plant.scientificName?: "No data"
+            tvDetailDescription.text = plant.description?: "No description data"
+            tvDetailCultivation.text = plant.cultivation?: "No cultivation data"
+            plant.family?.let { family ->
+                tvDetailFamily.text = family
+                tvDetailFamily.visibility = VISIBLE
+            }
         }
-
     }
 }
