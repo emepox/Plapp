e044c32494276ea63297031a6db78fce1943e5ad
mareikeherrmann <Mareike_Herrmann@hotmail.de>
2022-03-21 11:34:50 +0100

    slider label added

--- app/src/main/java/com/switcherette/plantapp/addPlant/view/PlantForm2Fragment.kt
+++ app/src/main/java/com/switcherette/plantapp/addPlant/view/PlantForm2Fragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.switcherette.plantapp.R
+import com.switcherette.plantapp.data.UserPlant
 import com.switcherette.plantapp.databinding.FragmentPlantForm2Binding
 
 class PlantForm2Fragment : Fragment(R.layout.fragment_plant_form2) {
@@ -20,56 +21,90 @@ class PlantForm2Fragment : Fragment(R.layout.fragment_plant_form2) {
         var finalUserPlant = args.userPlant
 
         with(binding) {
-            if (finalUserPlant?.water.isNullOrBlank()) {
-                slWater.setValue(3.0F)
-            } else {
+            setLabelsForWater()
 
-                finalUserPlant?.water?.let { slWater.value = it.toFloat() }
+            setLabelsForLight()
+
+            setRecommendedValuesLightAndWater(finalUserPlant)
+
+            btnSave.setOnClickListener {
+
+                val light = slLight.value
+                finalUserPlant = finalUserPlant?.copy(light = light.toString())
+
+                val water = slWater.value
+                finalUserPlant = finalUserPlant?.copy(water = water.toString())
+
             }
 
-            if (finalUserPlant?.light.isNullOrBlank()) {
-                slLight.setValue(3.0F)
-            } else {
-                finalUserPlant?.light?.let { slLight.value = it.toFloat() }
-            }
         }
-
 
     }
 
-
-/*            finalUserPlant?.water?.let { slWater.value = it.toFloat() }*/
-    /*   finalUserPlant?.light?.let { slLight.value = it.toFloat() }*/
-
-
-    /*etWater.setText(finalUserPlant?.water)
-    etLight.setText(finalUserPlant?.light)
-
-    btnSave.setOnClickListener {
-
-        etLight.text.toString().takeIf { it.isNotBlank() }?.let {
-            finalUserPlant = finalUserPlant?.copy(light = it)
-        } ?: run {
-            Toast.makeText(requireContext(), "please fill all fields", Toast.LENGTH_SHORT)
-                .show()
-            etLight.error = "Please fill this field"
+    private fun FragmentPlantForm2Binding.setRecommendedValuesLightAndWater(
+        finalUserPlant: UserPlant?
+    ) {
+        if (finalUserPlant?.water.isNullOrBlank()) {
+            slWater.value = 3.0F
+        } else {
+            finalUserPlant?.water?.let { slWater.value = it.toFloat() }
         }
 
-        etWater.text.toString().takeIf { it.isNotBlank() }?.let {
-            finalUserPlant = finalUserPlant?.copy(water = it)
-        } ?: run {
-            Toast.makeText(requireContext(), "please fill all fields", Toast.LENGTH_SHORT)
-                .show()
-            etWater.error = "Please fill this field"
+        if (finalUserPlant?.light.isNullOrBlank()) {
+            slLight.value = 3.0F
+        } else {
+            finalUserPlant?.light?.let { slLight.value = it.toFloat() }
         }
+    }
 
+    private fun FragmentPlantForm2Binding.setLabelsForWater() {
+        slWater.setLabelFormatter {
+            when (it.toInt()) {
+                1 -> "every 3 days"
+                2 -> "every week"
+                3 -> "every 2 weeks"
+                4 -> "every 3 weeks"
+                5 -> "monthly"
+                else -> "unknown"
+            }
+        }
+    }
 
-        finalUserPlant?.userId = Firebase.auth.currentUser?.uid!!
-
-    }*/
-
+    private fun FragmentPlantForm2Binding.setLabelsForLight() {
+        slLight.setLabelFormatter {
+            when (it.toInt()) {
+                1 -> "shadow"
+                2 -> "half shadow"
+                3 -> "medium"
+                4 -> "partly sun"
+                5 -> "direct sun"
+                else -> "unknown"
+            }
+        }
+    }
 }
 
-/*
-    }
-}*/
+/* btnSave.setOnClickListener {
+
+     etLight.text.toString().takeIf { it.isNotBlank() }?.let {
+         finalUserPlant = finalUserPlant?.copy(light = it)
+     } ?: run {
+         Toast.makeText(requireContext(), "please fill all fields", Toast.LENGTH_SHORT)
+             .show()
+         etLight.error = "Please fill this field"
+     }
+
+     etWater.text.toString().takeIf { it.isNotBlank() }?.let {
+         finalUserPlant = finalUserPlant?.copy(water = it)
+     } ?: run {
+         Toast.makeText(requireContext(), "please fill all fields", Toast.LENGTH_SHORT)
+             .show()
+         etWater.error = "Please fill this field"
+     }
+
+
+     finalUserPlant?.userId = Firebase.auth.currentUser?.uid!!
+
+
+}
+*/
