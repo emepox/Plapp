a92548495a7f781041d40270c42a974d85b01515
mareikeherrmann <Mareike_Herrmann@hotmail.de>
2022-03-21 17:39:18 +0100

    displaying icon for next water event is working

--- app/src/main/java/com/switcherette/plantapp/calendar/view/CalendarFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/calendar/view/CalendarFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.applandeo.materialcalendarview.EventDay
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener
 import com.switcherette.plantapp.R
@@ -12,10 +11,11 @@ import com.switcherette.plantapp.calendar.viewModel.CalendarViewModel
 import com.switcherette.plantapp.data.WaterEvent
 import com.switcherette.plantapp.databinding.FragmentCalendarBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.component.KoinComponent
 import java.text.SimpleDateFormat
 import java.util.*
 
-class CalendarFragment: Fragment(R.layout.fragment_calendar){
+class CalendarFragment : Fragment(R.layout.fragment_calendar), KoinComponent {
 
     private lateinit var binding: FragmentCalendarBinding
     private val calendarVM: CalendarViewModel by viewModel()
@@ -24,7 +24,7 @@ class CalendarFragment: Fragment(R.layout.fragment_calendar){
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCalendarBinding.bind(view)
 
-   /*     calendarVM.getWaterEvents()
+        calendarVM.getWaterEvents()
 
         calendarVM.waterEvents.observe(viewLifecycleOwner) { events ->
             if (events != null) {
@@ -33,21 +33,7 @@ class CalendarFragment: Fragment(R.layout.fragment_calendar){
             } else {
                 Toast.makeText(requireContext(), "Http error", Toast.LENGTH_SHORT).show()
             }
-        }*/
-
-        binding.calendarView.setOnDayClickListener(object : OnDayClickListener {
-            override fun onDayClick(eventDay: EventDay) {
-                val c = Calendar.getInstance()
-                c.time = eventDay.calendar.time
-                val millis = c.timeInMillis
-
-                val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-                val dateSimpleDayFormat = sdf.format(millis)
-
-                binding.tvSelectedDate.setText(dateSimpleDayFormat)
-            }
-        })
-
+        }
     }
 
     private fun convertEventsToEventDays(events: List<WaterEvent>): List<EventDay> {
@@ -55,18 +41,24 @@ class CalendarFragment: Fragment(R.layout.fragment_calendar){
             val calendar = Calendar.getInstance().apply {
                 time = Date(it.repeatStart)
             }
-            EventDay(calendar, R.drawable.ic_waterdrop)
+            EventDay(calendar, R.drawable.ic_flower)
         }
     }
 
     private fun fillCalendarWithEvents(events: List<EventDay>) {
         binding.calendarView.apply {
             setEvents(events)
+
             setOnDayClickListener(object : OnDayClickListener {
                 override fun onDayClick(eventDay: EventDay) {
                     val c = Calendar.getInstance()
                     c.time = eventDay.calendar.time
-                   // val millis = c.timeInMillis
+                    val millis = c.timeInMillis
+
+                    val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+                    val dateSimpleDayFormat = sdf.format(millis)
+
+                    binding.tvSelectedDate.setText(dateSimpleDayFormat)
                 }
             })
         }
