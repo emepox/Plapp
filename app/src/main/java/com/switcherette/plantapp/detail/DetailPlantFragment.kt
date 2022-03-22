6ef2f66870666e045a74fd0d518d17235869cadf
MariaVivancos <mariavivancos@maria.vivancos>
2022-03-22 17:22:44 +0100

    Edit functionality added and working. Needs improvement.

--- app/src/main/java/com/switcherette/plantapp/detail/DetailPlantFragment.kt
+++ app/src/main/java/com/switcherette/plantapp/detail/DetailPlantFragment.kt
@@ -1,6 +1,8 @@
 package com.switcherette.plantapp.detail
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
@@ -24,6 +26,7 @@ class DetailPlantFragment : Fragment(R.layout.fragment_detail_plant) {
 
     private lateinit var binding: FragmentDetailPlantBinding
     private lateinit var plant: UserPlant
+    private lateinit var editedPlant : UserPlant
 
     private var saveIcon : Boolean = false
 
@@ -69,36 +72,35 @@ class DetailPlantFragment : Fragment(R.layout.fragment_detail_plant) {
 
         with(binding) {
 
-
-            tvDetailNickname.text = plant.nickname
+            etDetailNickname.setText(plant.nickname)
             tvDetailCommonName.text = plant.commonName?: "No common name data"
 
-            tvCareWatering.text = "every ${plant.water} days"
+            etCareWatering.setText("every ${plant.water} days")
             plant.light.let {
                 when(it) {
                     1 -> {
-                        tvCareLight.text = "Full shadow"
+                        etCareLight.setText("Full shadow")
                         ivCareLight.setImageResource(R.drawable.ic_cloud)
                     }
                     2 -> {
-                        tvCareLight.text = "Partial shade"
+                        etCareLight.setText("Partial shade")
                         ivCareLight.setImageResource(R.drawable.ic_sun_clouds)
                     }
                     3 -> {
-                        tvCareLight.text= "Full sun"
+                        etCareLight.setText("Full sun")
                         ivCareLight.setImageResource(R.drawable.ic_sun)
                     }
                     4 -> {
-                        tvCareLight.text = "Light adaptable"
+                        etCareLight.setText("Light adaptable")
                         ivCareLight.setImageResource(R.drawable.ic_sun_clouds)
                     }
-                    else -> tvCareLight.text = "No data"
+                    else -> etCareLight.setText("No data")
 
                 }
             }
             tvDetailScientificName.text = plant.scientificName?: "No data"
-            tvDetailDescription.text = plant.description?: "No description data"
-            tvDetailCultivation.text = plant.cultivation?: "No cultivation data"
+            etDetailDescription.setText(plant.description?: "No description data")
+            etDetailCultivation.setText(plant.cultivation?: "No cultivation data")
             plant.family?.let { family ->
                 tvDetailFamily.text = family
                 tvDetailFamily.visibility = VISIBLE
@@ -123,15 +125,45 @@ class DetailPlantFragment : Fragment(R.layout.fragment_detail_plant) {
             saveIcon = !saveIcon
             Log.e("saveiconafter", "$saveIcon")
             if (saveIcon) {
-
                 // EDIT FUNCTIONALITY
-                Toast.makeText(requireContext(), "Clicked edit", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Please edit", Toast.LENGTH_SHORT).show()
                 btnEditPlant.setImageResource(R.drawable.ic_save)
+
+                with(binding) {
+                    mutableListOf(
+                        etDetailNickname,
+                        etDetailDescription,
+                        etDetailCultivation
+                    ).onEach { element ->
+                        element.isEnabled = true
+                        element.setBackgroundColor(Color.parseColor("#FF9C4141"))
+                        //it.setHintTextColor("#FF9C4141")
+                        //it.setTextAppearance(R.style.LineEditText)
+                    }
+                }
             } else {
                 // SAVE FUNCTIONALITY
                 Toast.makeText(requireContext(), "Clicked save", Toast.LENGTH_SHORT).show()
-
                 btnEditPlant.setImageResource(R.drawable.ic_edit2)
+
+                // Create new edited plant object
+                with(binding) {
+                    editedPlant = UserPlant(
+                        plant.id,
+                        etDetailNickname.text.toString(),
+                        plant.scientificName,
+                        plant.commonName,
+                        plant.family,
+                        etDetailDescription.text.toString(),
+                        etDetailCultivation.text.toString(),
+                        plant.light,
+                        plant.water,
+                        plant.image,
+                        plant.userId
+                    )
+                    Log.e("editedplant", "$editedPlant")
+                    detailPlantVM.editPlant(editedPlant)
+                }
             }
         }
 
@@ -140,8 +172,6 @@ class DetailPlantFragment : Fragment(R.layout.fragment_detail_plant) {
             findNavController().navigate(R.id.action_detailPlantFragment_to_myPlantsFragment)
 
         }
-
-
     }
 
     private fun onEditButtonClicked() {
